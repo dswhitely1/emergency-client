f73dcd69f3954ee897642324ca1da5f8fcc5dd5d
Donald Whitely <dswhitely1@gmail.com>
2020-12-03 21:44:09 -0500

    Removed Yarn.lock

--- src/components/landing-page/pages/Footer.jsx
+++ src/components/landing-page/pages/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Grid, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import {
   StyledContainer,
   StyledFooter,
